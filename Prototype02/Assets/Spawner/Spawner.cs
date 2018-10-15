1e98dd7772ffd0bc41c1cf1c5d751073891b00ec
Belu123mm <32393295+Belu123mm@users.noreply.github.com>
2018-10-15 18:21:47 -0300

    Revert "Ps tiemr hecho"
    This reverts commit ef766968987f69834e1e2b18ee5ed7407b9b2718.

---
+++ Prototype02/Assets/Spawner/Spawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+    public List<Wave> oleadas;
+    public List<NodeGroup> nodeGroups;
+    public float waitToStartTime;
+    public float timer;
+    public bool start;
+    public bool finished;
+    public Transform target;
+
+    public void Update() {
+        timer += Time.deltaTime;
+        if ( timer > waitToStartTime ) start = true;
+        //Hasta aqui todo perfecto
+        if ( start && !finished) {
+            foreach ( var w in oleadas ) {
+                for ( int i = 0; i < w.qt; i++ ) {
+                    print(i);
+                    Turret en = w.enemy;
+                    en.target = target;
+                    if ( w.hasNodes )
+                        en.nodegroup = nodeGroups [ 0 ];
+                    Instantiate(en);
+                }
+            finished = true;
+            start = false;
+            }
+        }
+        //asi funciona pero sin timers
+    }
+}
