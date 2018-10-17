de88ae56ee635aea77b37a6e2d2f87c89feb745a
Belu123mm <32393295+Belu123mm@users.noreply.github.com>
2018-10-17 13:23:57 -0300

    lo ultimo de esto wey

--- BeluPrototype/Assets/Scripts/Enemies/Turret/SimpleTurret.cs
+++ BeluPrototype/Assets/Scripts/Enemies/Turret/SimpleTurret.cs
@@ -28,18 +28,22 @@ public class SimpleTurret : Turret, IEnemy {
         var moving = new State<TurretState>("MOVE");
         var shooting = new State<TurretState>("SHOOT");
         var iterate = new State<TurretState>("ITERATE");
+        var quiet = new State<TurretState>("QUIET");
 
         moving.AddTransition(TurretState.shooting, shooting);
         moving.AddTransition(TurretState.iterate, iterate);
         shooting.AddTransition(TurretState.moving, moving);
         shooting.AddTransition(TurretState.iterate, iterate);
+        shooting.AddTransition(TurretState.quiet, quiet);
         iterate.AddTransition(TurretState.moving, moving);
         iterate.AddTransition(TurretState.shooting, shooting);
+        quiet.AddTransition(TurretState.shooting, shooting);
 
         moving.OnUpdate = () => navigation.MoveToNext();
 
         shooting.OnUpdate = () => Shoot();
 
+        if(hasNavigation)
         iterate.OnEnter = () => navigation.NextNode();
 
         stateMachine = new EventFSM<TurretState>(iterate);
@@ -49,8 +53,13 @@ public class SimpleTurret : Turret, IEnemy {
 
     // Update is called once per frame
     void Update() {
-
-        if ( navigation.currentNode ) {
+        if (!hasNavigation) {
+            if ( sight.GetSight() )
+                OnInput(TurretState.shooting);
+            else
+                OnInput(TurretState.quiet);
+        }
+        else if ( navigation.currentNode ) {
             if ( navigation.IsOnDistance() ) {
                 OnInput(TurretState.iterate);
             } else
@@ -68,6 +77,7 @@ public class SimpleTurret : Turret, IEnemy {
             bulletGenerator.Shoot();
             timer = 0;
         }
+        if (hasNavigation)
         navigation.mOVEbUTsLOWER();
     }
 
