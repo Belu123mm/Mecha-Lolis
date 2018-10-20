d1f1edb4b2add4e671bce39f52a214a26085723d
Belu123mm <32393295+Belu123mm@users.noreply.github.com>
2018-10-20 14:47:24 -0300

    animaciontest

--- BeluPrototype/Assets/Scripts/Enemies/Turret/RoundTurret.cs
+++ BeluPrototype/Assets/Scripts/Enemies/Turret/RoundTurret.cs
@@ -26,27 +26,57 @@ public class RoundTurret : Turret, IEnemy {
     public void Update() {
         timer += Time.deltaTime;
 
-        if ( sight.GetSight() ) {
-            OnInput(TurretState.shooting);
-        } else {
-            OnInput(TurretState.quiet);
-        }
 
+        if ( !hasNavigation ) {
+            if ( sight.GetSight() )
+                OnInput(TurretState.shooting);
+            else
+                OnInput(TurretState.quiet);
+        } else if ( navigation.currentNode ) {
+            if ( navigation.IsOnDistance() ) {
+                OnInput(TurretState.iterate);
+            } else
+            if ( sight.GetSight() ) {
+                OnInput(TurretState.shooting);
+            } else {
+                OnInput(TurretState.moving);
+            }
+        }
         stateMachine.Update();
+        Debug.Log(stateMachine.current.name);
+        Debug.Log(hasNavigation);
+
 
     }
 
     public void SetStateMachine() {
+        var iterate = new State<TurretState>("ITERATE");
         var quiet = new State<TurretState>("QUIET");
         var shooting = new State<TurretState>("SHOOT");
+        var moving = new State<TurretState>("MOVING");
 
         quiet.AddTransition(TurretState.shooting, shooting);
+        quiet.AddTransition(TurretState.moving, moving);
+        quiet.AddTransition(TurretState.iterate, iterate);
 
         shooting.AddTransition(TurretState.quiet, quiet);
+        shooting.AddTransition(TurretState.moving, moving);
+        shooting.AddTransition(TurretState.iterate, iterate);
+
+        moving.AddTransition(TurretState.moving, shooting);
+        moving.AddTransition(TurretState.iterate, iterate);
+
+        iterate.AddTransition(TurretState.moving, moving);
+        iterate.AddTransition(TurretState.shooting, shooting);
+
 
         quiet.OnUpdate += () => Quiet();
 
         shooting.OnUpdate += () => Shoot();
+
+        if ( hasNavigation )
+            iterate.OnEnter = () => navigation.NextNode();
+
 
         stateMachine = new EventFSM<TurretState>(quiet);
 
@@ -57,6 +87,9 @@ public class RoundTurret : Turret, IEnemy {
             bulletGenerator.Shoot();
             timer = 0;
         }
+        if ( hasNavigation )
+            navigation.mOVEbUTsLOWER();
+
     }
     public void Quiet() {
 
